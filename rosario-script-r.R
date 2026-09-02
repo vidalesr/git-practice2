18fe9d3827a1ccb1c33f2d453e5525c86b16c298
vidalesr <rosario.vidales006@gmail.com>
2026-09-02 16:27:09 -0400

    edit in branch 1

--- rosario-script-r.R
+++ rosario-script-r.R
@@ -13,3 +13,5 @@
 # visualization -------------------------------------------------------------------
 
 # what happens when I pull before committing and there were changes pulled?
+
+# making an edit in branch 1
